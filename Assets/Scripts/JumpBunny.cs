a714460d20c303c4aff4deb89e7a88e0f1bea6e4
Khushi Agarwal <khushiagarwal0320@gmail.com>
2021-08-19 18:17:15 +0530

    game

---
+++ Assets/Scripts/JumpBunny.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpBunny : MonoBehaviour
+{
+    //Refrence to GameManager Script
+    public GameManager gameManager;
+
+    public float velocity = 1;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0) || Input.GetKeyDown("space"))
+        {
+            // Bunny Jumps
+            rb.velocity = Vector2.up * velocity;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        gameManager.GameOver(); // Trigger the gameover method
+    }
+
+}
---
+++ Assets/Scripts/PipeSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    public float maxTime = 5;
+    private float timer = 0;
+
+    // Reference to Pipe
+    public GameObject bigpipe;
+    public float height;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newbigpipe = Instantiate(bigpipe);
+        newbigpipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > maxTime)
+        {
+            GameObject newbigpipe = Instantiate(bigpipe);
+            newbigpipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+            Destroy(newbigpipe, 15);
+            timer = 0;
+        }
+
+        timer += Time.deltaTime;
+    }
+}
